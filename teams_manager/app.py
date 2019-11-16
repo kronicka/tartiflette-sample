7657a853ef96348d4f801bb79c4b9cc9c6cdadff
Ana Shevchenko <kronicka@protonmail.com>
2019-11-16 18:48:30 +0200

    Change Query objects to something more custom, make the app runnable

---
+++ teams_manager/app.py
@@ -0,0 +1,26 @@
+import os
+
+from aiohttp import web
+from tartiflette_aiohttp import register_graphql_handlers
+
+
+def run() -> None:
+    """
+    Entry point of the app.
+    """
+    web.run_app(
+        register_graphql_handlers(
+            app=web.Application(),
+            engine_sdl=os.path.dirname(os.path.abspath(__file__)) + "/sdl",
+            engine_modules=[
+                "teams_manager.query_resolvers",
+                "teams_manager.mutation_resolvers",
+                "teams_manager.subscription_resolvers",
+                "teams_manager.directives.rate_limiting",
+                "teams_manager.directives.auth"
+            ],
+            executor_http_endpoint="/graphql",
+            executor_http_methods=["POST"],
+            graphiql_enabled=True,
+        )
+    )
---
+++ teams_manager/data.py
@@ -0,0 +1,27 @@
+# Members based on the team ID as key.
+
+MEMBERS = {
+    1: [
+        {"firstName": "Hari", "lastName": "Seldon", "experienceLevel": "PRO", "reactionTime": 120},
+        {"firstName": "Noah", "lastName": "Blumenfeld", "experienceLevel": "PRO", "reactionTime": 175},
+        {"firstName": "Harriet", "lastName": "Stowe", "experienceLevel": "EXPERIENCED", "reactionTime": 253},
+    ],
+    2: [
+        {"firstName": "Dio", "lastName": "Brando", "experienceLevel": "PRO", "reactionTime": 150},
+        {"firstName": "Peter", "lastName": "Parker", "experienceLevel": "PRO", "reactionTime": 180},
+        {"firstName": "Hurdy", "lastName": "Gurdy", "experienceLevel": "EXPERIENCED", "reactionTime": 260},
+        {"firstName": "Ana", "lastName": "Banana", "experienceLevel": "ROOKIE", "reactionTime": 330},
+
+    ],
+    3: [
+        {"firstName": "Ada", "lastName": "Lovelace", "experienceLevel": "PRO", "reactionTime": 133},
+        {"firstName": "Carl", "lastName": "Sjoberg", "experienceLevel": "PRO", "reactionTime": 181},
+        {"firstName": "Polter", "lastName": "Geist", "experienceLevel": "ROOKIE", "reactionTime": 323},
+    ],
+}
+
+TEAMS = [
+    {"id": 1, "name": "Cubes", "motto": "Be there or be square."},
+    {"id": 2, "name": "Filthy Acts at a Reasonable Price", "motto": "Our name is definitely not a JoJo reference."},
+    {"id": 3, "name": "Worse Company", "motto": "Imagine not using comparative adjectives."},
+]
